95dc66cca4d67676c24de7daccf29a7ecca706db
likeanowl <svitkovsergey@gmail.com>
2018-09-21 18:58:23 +0300

    Added mapping methods to it's ranges. Not tested yet

---
+++ src/main/java/diff/FileMapper.java
@@ -0,0 +1,46 @@
+package diff;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaRecursiveElementVisitor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiMethod;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public final class FileMapper {
+
+    //Method name (like org.smth.ImbaClass.getSmth) to it boundaries
+    final private Map<PsiMethod, TextRange> methodToBounds;
+    final private PsiManager pm;
+
+    public FileMapper(Project project, List<VirtualFile> files) {
+        methodToBounds = new HashMap<>();
+        this.pm = PsiManager.getInstance(project);
+    }
+
+    private void init(List<VirtualFile> files) {
+        files.parallelStream()
+                .map(pm::findFile)
+                .filter(Objects::nonNull)
+                .forEach(x -> x.acceptChildren(new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitElement(PsiElement element) {
+                if (element instanceof PsiMethod)
+                    methodToBounds.put((PsiMethod) element, element.getTextRange());
+
+                super.visitElement(element);
+            }
+        }));
+    }
+
+    public Map<PsiMethod, TextRange> getMethodToBounds() {
+        return methodToBounds;
+    }
+}
+
