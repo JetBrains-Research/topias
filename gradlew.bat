22bb8ba9929d9fe1287aa9b3bfb89f7ece363428
likeanowl <svitkovsergey@gmail.com>
2020-02-03 21:42:51 +0300

    Add gradle opts in gradlew script

--- gradlew.bat
+++ gradlew.bat
@@ -81,6 +81,8 @@ set CMD_LINE_ARGS=%*
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+set GRADLE_OPTS="\-Dorg.gradle.jvmargs='-Xmx2048m -XX:MaxPermSize=512m'"
+
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
